c85e52c88a04bac943c2beabbcea016047815c96
Peter Kvokačka <peter.kvokacka11@gmail.com>
2025-12-08 10:59:26 +0100

    Refactor IP range in VerifyCronRequest middleware and enhance logging; update cron routes for better organization

--- app/Http/Middleware/VerifyCronRequest.php
+++ app/Http/Middleware/VerifyCronRequest.php
@@ -19,21 +19,21 @@ class VerifyCronRequest
         $clientIp = ip2long($request->ip()); // Prevod IP adresy na číselný formát
 
         // Definuj rozsah IP adries (od-do)
-        $startIp = ip2long('37.9.175.155');
-        $endIp = ip2long('37.9.175.157');
+        $startIp = ip2long('10.31.29.14');
+        $endIp = ip2long('10.31.29.14');
 
-        // Log::info('Client IP:', ['ip' => $request->ip()]);
-        // Log::info('Client IP:', ['ip' => $clientIp]);
-        // Log::info('Start IP:', ['start_ip' => $startIp]);
-        // Log::info('End IP:', ['end_ip' => $endIp]);
+        Log::info('Client IP:', ['ip' => $request->ip()]);
+        Log::info('Client IP:', ['ip' => $clientIp]);
+        Log::info('Start IP:', ['start_ip' => $startIp]);
+        Log::info('End IP:', ['end_ip' => $endIp]);
 
         // Skontroluj, či IP adresa spadá do rozsahu
         if ($clientIp < $startIp || $clientIp > $endIp) {
-            // Log::warning('Access denied for IP:', ['ip' => $request->ip()]);
+            Log::warning('Access denied for IP:', ['ip' => $request->ip()]);
             abort(403, 'Access denied.');
         }
 
-        // Log::info('Access granted for IP:', ['ip' => $request->ip()]);
+        Log::info('Access granted for IP:', ['ip' => $request->ip()]);
 
         return $next($request);
     }
--- routes/cron.php
+++ routes/cron.php
@@ -16,13 +16,15 @@ Route::middleware('cron')->group(function () {
         Artisan::call('queue:restart');
         Artisan::call('queue:work');
     });
+    
     Route::get('cronDATES', [CronJobVotingDatesController::class, 'index'])->name('cronJOBvotesDATES.index');
     Route::get('cronVOTED', [CronJobVotedSongsController::class, 'index'])->name('cronJOBvotedSONGS.index');
-    Route::get('cronIMAGE', [ImageGeneratorController::class, 'index'])->name('cronImage.index');
-    
     Route::get('cronSONGS', [CronJobVotingSongsController::class, 'index'])->name('cronJOBvotesSONGS.index');
+    
     Route::get('cronAddSONGS', [CronJobAddSongsController::class, 'index'])->name('cronJOBaddSONGS.index');
-    Route::get('cronCreateImage', [CronJobCreateVoteResultImg::class, 'index'])->name('cronJOBcreateImage.index');
     Route::get('cronPlaySongs', [CronJobPlaySongsController::class, 'index'])->name('cronJOBplaySongs.index');
     Route::get('cronPauseSongs', [CronJobPauseSongsController::class, 'index'])->name('cronJOBpauseSongs.index');
+    
+    Route::get('cronIMAGE', [ImageGeneratorController::class, 'index'])->name('cronImage.index');
+    Route::get('cronCreateImage', [CronJobCreateVoteResultImg::class, 'index'])->name('cronJOBcreateImage.index');
 });
