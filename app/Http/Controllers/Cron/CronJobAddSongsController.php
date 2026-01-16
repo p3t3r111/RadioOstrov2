3bf95e8d5f750aa4ee6da4e2d1b57660f4125a57
Peter Kvokačka <peter.kvokacka11@gmail.com>
2026-01-16 13:36:02 +0100

    Refactor CronJobAddSongsController for code consistency; improve formatting and enhance track addition logic

--- app/Http/Controllers/Cron/CronJobAddSongsController.php
+++ app/Http/Controllers/Cron/CronJobAddSongsController.php
@@ -7,8 +7,9 @@ use App\Models\Vote;
 use Carbon\Carbon;
 use Exception;
 use Illuminate\Support\Facades\DB;
-use SpotifyWebAPI\SpotifyWebAPI; //https://github.com/jwilsson/spotify-web-api-php
-use SpotifyWebAPI\Session;
+use Illuminate\Support\Str;
+use SpotifyWebAPI\Session; // https://github.com/jwilsson/spotify-web-api-php
+use SpotifyWebAPI\SpotifyWebAPI;
 
 class CronJobAddSongsController extends Controller
 {
@@ -17,7 +18,7 @@ class CronJobAddSongsController extends Controller
         $playlist_id = config('spotify.playlist_id');
 
         $datum = Carbon::now()->format('Y-m-d');
-        $db_query = Vote::select('songId', 'datum', DB::raw('count(id) as voteCount'))->where('datum', $datum)->groupBy("songId", "datum")->orderBy("voteCount", "DESC")->get();
+        $db_query = Vote::select('songId', 'datum', DB::raw('count(id) as voteCount'))->where('datum', $datum)->groupBy('songId', 'datum')->orderBy('voteCount', 'DESC')->get();
 
         $session = new Session(
             config('spotify.client_id'),
@@ -26,23 +27,22 @@ class CronJobAddSongsController extends Controller
 
         $session->refreshAccessToken(config('spotify.refresh_token'));
         $accessToken = $session->getAccessToken();
-        $api = new SpotifyWebAPI();
+        $api = new SpotifyWebAPI;
         $api->setAccessToken($accessToken);
-
 
         // VYMAZANIE PLAYLISTU
         $track_uris = [];
         $tracks = $api->getPlaylistTracks($playlist_id);
         foreach ($tracks->items as $item) {
-            if (isset($item->track) && isset($item->track->uri)) {
-                $track_uris[] = ["uri" => $item->track->uri];
+            if (isset($item->track) && isset($item->track->uri) && ! Str::contains($item->track->name, 'radioSpeech')) {
+                $track_uris[] = ['uri' => $item->track->uri];
             }
         }
         dump($track_uris);
         if (count($track_uris) > 0) {
 
             $request_body = [
-                'tracks' => $track_uris
+                'tracks' => $track_uris,
             ];
             $api->deletePlaylistTracks($playlist_id, $request_body);
         }
@@ -50,18 +50,18 @@ class CronJobAddSongsController extends Controller
         $datumNow = Carbon::now()->format('d.m.Y');
 
         $api->updatePlaylist($playlist_id, [
-            'name' => 'Radio ostrov | ' . $datumNow,
+            'name' => 'Radio ostrov | '.$datumNow,
         ]);
 
         // PRIDAVANIE pesničiek
 
         foreach ($db_query as $index => $song) {
-            $uri = "spotify:track:" . $song->songId;
+            $uri = 'spotify:track:'.$song->songId;
             try {
-                $vysledok = $api->addPlaylistTracks($playlist_id, [$uri], ['position' => $index]);
+                $vysledok = $api->addPlaylistTracks($playlist_id, [$uri], ['position' => $index++]);
                 echo 'Skladba bola úspešne pridaná!';
             } catch (Exception $e) {
-                echo 'Chyba: ' . $e->getMessage();
+                echo 'Chyba: '.$e->getMessage();
             }
         }
     }
