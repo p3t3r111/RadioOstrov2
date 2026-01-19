17b75ce9b449fe05fe1840d0326c888aeba78a0d
Peter Kvokačka <peter.kvokacka11@gmail.com>
2026-01-19 21:50:01 +0100

    Refactor song selection logic in CronJobVotingSongsController; ensure only unplayed confirmed songs are considered for voting

--- app/Http/Controllers/Cron/CronJobVotingSongsController.php
+++ app/Http/Controllers/Cron/CronJobVotingSongsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Cron;
 
 use App\Http\Controllers\Controller;
 use App\Models\Active_voting_song;
+use App\Models\Backup_song;
 use App\Models\Song;
 use App\Models\User;
 
@@ -14,8 +15,9 @@ class CronJobVotingSongsController extends Controller
         User::where('voted', 1)->update(['voted' => 0]);
 
         $songs = 0;
-        $songs = Song::where('confirmed', 1)->get();
+        $songs = Song::where('confirmed', 1)->where('weekly_played', 0)->get();
         $songsCount = count($songs);
+        Active_voting_song::truncate();
         if ($songsCount > 0) {
             // user songs
             $songVotesList = Song::where('confirmed', 1)
@@ -23,18 +25,19 @@ class CronJobVotingSongsController extends Controller
                 ->inRandomOrder()
                 ->limit(10)
                 ->get();
-
-            Active_voting_song::truncate();
-
-            foreach ($songVotesList as $item) {
-                Active_voting_song::create([
-                    'songId' => trim($item->songId),
-                    'imgPath' => trim($item->img_path),
-                    'author' => trim($item->author),
-                    'title' => trim($item->title),
-                ]);
-            }
         } else {
+            $songVotesList = Backup_song::where('weekly_played', 0)
+                ->inRandomOrder()
+                ->limit(10)
+                ->get();
+        }
+        foreach ($songVotesList as $item) {
+            Active_voting_song::create([
+                'songId' => trim($item->songId),
+                'imgPath' => trim($item->img_path),
+                'author' => trim($item->author),
+                'title' => trim($item->title),
+            ]);
         }
 
         dd('done');
