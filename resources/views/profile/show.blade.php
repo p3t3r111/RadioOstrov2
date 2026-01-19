ff60226f32d78d039c75bbe3426acb8ae2d65c57
Peter Kvokačka <peter.kvokacka11@gmail.com>
2026-01-19 19:39:27 +0100

    Comment out the update password form section in the profile view for future use

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -21,11 +21,11 @@ Rádio ostrov | {{ auth()->user()->name }}
             </div>
         </div>
 
-        <div class="p-4 sm:p-8 bg-white dark:bg-gray-300 shadow sm:rounded-lg">
+        {{-- <div class="p-4 sm:p-8 bg-white dark:bg-gray-300 shadow sm:rounded-lg">
             <div class="max-w-xl">
                 @include('profile.partials.update-password-form')
             </div>
-        </div>
+        </div> --}}
 
         <div class="p-4 sm:p-8 bg-white dark:bg-gray-300 shadow sm:rounded-lg">
             <div class="max-w-xl">
