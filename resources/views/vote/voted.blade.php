d1bb7220d9bd3bc0208d668da8a5dd5d21dbb446
Peter Kvokačka <peter.kvokacka11@gmail.com>
2026-01-21 22:34:48 +0100

    Refactor vote views; enhance layout and messaging for no active votes and post-vote confirmation

--- resources/views/vote/voted.blade.php
+++ resources/views/vote/voted.blade.php
@@ -1,14 +1,37 @@
 @extends('layouts.main')
 
 @section('title')
-Rádio ostrov | Hlasovanie
+    Rádio ostrov | Hlasovanie
 @endsection
 
 @section('content')
+    <section
+        class="flex-1 bg-gradient-to-br from-green-500 to-green-600 text-white flex flex-col items-center justify-center gap-4">
 
-<section class="pt-12 h-[85vh] bg-green-500 text-white flex flex-col items-center justify-center gap-2">
-  <img src="{{ asset("assets/logo-white.png") }}" alt="Logo školy : Stredná odborná škola informačných technológií, Ostrovského 1, Košice">
-  <h2>RÁDIO OSTROV</h2>
-  <p>Ďakujeme za hlasovanie.</p>
-</section>
+        <div class="flex flex-col items-center">
+            <img src="{{ asset('assets/logo-white.png') }}"
+                alt="Logo školy : Stredná odborná škola informačných technológií, Ostrovského 1, Košice"
+                class="w-24 h-auto mb-2">
+
+            <h2 class="text-2xl font-bold tracking-wide">
+                Rádio Ostrov
+            </h2>
+
+            <p class="text-lg font-semibold">
+                Ďakujeme za hlasovanie!
+            </p>
+        </div>
+
+        <div class="flex flex-col items-center">
+            <p class=" text-white/80">
+                Môžeš si skontrolovať profil a pridať obľúbené pesničky,
+                ak ich tam ešte nemáš.
+            </p>
+
+            <a href="{{ route('profile.show') }}"
+                class="px-3 py-2 lg:mt-5 lg:px-8 lg:py-4 text-black text-center rounded-lg shadow-lg uppercase bg-white">
+                Môj profil
+            </a>
+        </div>
+    </section>
 @endsection
