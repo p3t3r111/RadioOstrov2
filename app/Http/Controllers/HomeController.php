2ad4779bb8eddbf2e00a35f1bc4639e1dce094c9
Peter Kvokačka <peter.kvokacka11@gmail.com>
2026-01-22 22:46:14 +0100

    Refactor voting date handling in HomeController and update index view for dynamic display

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,6 +12,7 @@ class HomeController extends Controller
     {
         $dayOfWeek = null;
         $canVote = Vote::canVote();
+        $exportDates = [];
 
         $dateNow = Carbon::now()->format('Y-m-d H:i:s');
         $dateNowUNIX = strtotime(datetime: $dateNow);
@@ -23,34 +24,30 @@ class HomeController extends Controller
             $fromUNIX = strtotime($dateInterval['from']);
             $toUNIX = strtotime($dateInterval['to']);
 
+            $fromDate = $dateInterval['from']->format('d.m.Y');
+            $toDate = $dateInterval['to']->format('d.m.Y');
+
+            // $dateNames = Carbon::getDays();
+            $datesName = [
+                0 => 'Pondelok',
+                1 => 'Utorok',
+                2 => 'Streda',
+                3 => 'Štvrtok',
+                4 => 'Piatok',
+            ];
+
             if ($dateNowUNIX >= $fromUNIX && $dateNowUNIX <= $toUNIX) {
                 $votingDateUNIX = $toUNIX + 86400;
                 $dayOfWeek = date('w', $votingDateUNIX);
-                break;
             }
+
+            $exportDates[] = [
+                'from' => $fromDate,
+                'to' => $toDate,
+                'name' => $datesName[count($exportDates)],
+            ];
         }
 
-        $mondayFROM = $dateIntervals[0]['from'];
-        $mondayFROM = Carbon::createFromFormat('Y-m-d', $mondayFROM)->format('d.m.Y');
-        $mondayTO = $dateIntervals[0]['to'];
-        $mondayTO = Carbon::createFromFormat('Y-m-d', $mondayTO)->format('d.m.Y');
-        $tuesdayFROM = $dateIntervals[1]['from'];
-        $tuesdayFROM = Carbon::createFromFormat('Y-m-d', $tuesdayFROM)->format('d.m.Y');
-        $tuesdayTO = $dateIntervals[1]['to'];
-        $tuesdayTO = Carbon::createFromFormat('Y-m-d', $tuesdayTO)->format('d.m.Y');
-        $wednesdayFROM = $dateIntervals[2]['from'];
-        $wednesdayFROM = Carbon::createFromFormat('Y-m-d', $wednesdayFROM)->format('d.m.Y');
-        $wednesdayTO = $dateIntervals[2]['to'];
-        $wednesdayTO = Carbon::createFromFormat('Y-m-d', $wednesdayTO)->format('d.m.Y');
-        $thursdayFROM = $dateIntervals[3]['from'];
-        $thursdayFROM = Carbon::createFromFormat('Y-m-d', $thursdayFROM)->format('d.m.Y');
-        $thursdayTO = $dateIntervals[3]['to'];
-        $thursdayTO = Carbon::createFromFormat('Y-m-d', $thursdayTO)->format('d.m.Y');
-        $fridayFROM = $dateIntervals[4]['from'];
-        $fridayFROM = Carbon::createFromFormat('Y-m-d', $fridayFROM)->format('d.m.Y');
-        $fridayTO = $dateIntervals[4]['to'];
-        $fridayTO = Carbon::createFromFormat('Y-m-d', $fridayTO)->format('d.m.Y');
-
-        return View('index', ['canVote' => $canVote, 'activeVotingDay' => $dayOfWeek, 'mondayFrom' => $mondayFROM, 'tuesdayFrom' => $tuesdayFROM, 'wednesdayFrom' => $wednesdayFROM, 'thursdayFrom' => $thursdayFROM, 'fridayFrom' => $fridayFROM, 'mondayTo' => $mondayTO, 'tuesdayTo' => $tuesdayTO, 'wednesdayTo' => $wednesdayTO, 'thursdayTo' => $thursdayTO, 'fridayTo' => $fridayTO]);
+        return View('index', ['canVote' => $canVote, 'activeVotingDay' => $dayOfWeek, 'votingDates' => $exportDates]);
     }
 }
