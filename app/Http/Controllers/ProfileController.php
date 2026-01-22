00daf17262c7981ebc9352ba4622b9760ca9372d
Peter Kvokačka <peter.kvokacka11@gmail.com>
2026-01-22 15:05:36 +0100

    Add user voting details to profile; include votes, invited_people, max_votes_per_day, and weight_per_vote

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -25,6 +25,10 @@ class ProfileController extends Controller
         return view('profile.show', [
             'user' => $request->user(),
             'songs' => $user->songs,
+            'votes' => $user->votes,
+            'invited_people' => $user->invited_people,
+            'max_votes_per_day' => $user->max_votes_per_day,
+            'weight_per_vote' => $user->vote_weight,
         ]);
     }
 
--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -5,23 +5,29 @@
                 {{ __('Informácie o profile') }}
             </h2>
         </header>
-        <div class="flex flex-col lg:flex-row grow gap-4">
-            <div class="flex-1" >
+        <div class="grid grid-cols-4 gap-4">
+            <div class="flex-1 col-span-2">
                 <label class="block font-medium text-sm text-black" for="name">
                     Meno
                 </label>
-                <input disabled="" class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm mt-1 block w-full cursor-not-allowed" id="name" name="name" type="text" value="{{ $user->name }}" required="required" autofocus="autofocus" autocomplete="name">
+                <input disabled=""
+                    class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm mt-1 block w-full cursor-not-allowed"
+                    id="name" name="name" type="text" value="{{ $user->name }}" required="required"
+                    autofocus="autofocus" autocomplete="name">
                 {{-- <x-input-error class="mt-2" :messages="$errors->get('name')" /> --}}
             </div>
-        
-            <div class="flex-1">
+
+            <div class="flex-1 col-span-2">
                 <label class="block font-medium text-sm text-black" for="email">
                     Email
                 </label>
-                <input disabled="" class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm mt-1 block w-full cursor-not-allowed" id="email" name="email" type="email" value="{{ $user->email }}" required="required" autocomplete="username">
+                <input disabled=""
+                    class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm mt-1 block w-full cursor-not-allowed"
+                    id="email" name="email" type="email" value="{{ $user->email }}" required="required"
+                    autocomplete="username">
                 {{-- <x-input-error class="mt-2" :messages="$errors->get('email')" /> --}}
-        
-                {{-- @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && ! $user->hasVerifiedEmail())
+
+                {{-- @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && !$user->hasVerifiedEmail())
                     <div>
                         <p class="text-sm mt-2 text-gray-800 dark:text-gray-200">
                             {{ __('Your email address is unverified.') }}
@@ -39,6 +45,44 @@
                     </div>
                 @endif --}}
             </div>
+
+            <div class="flex-1">
+                <label class="block font-medium text-sm text-black" for="invited_people">
+                    Pozvaní ľudia
+                </label>
+                <input disabled=""
+                    class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm mt-1 block w-full cursor-not-allowed"
+                    id="invited_people" name="invited_people" type="text" value="{{ $invited_people }}"
+                    required="required" autocomplete="invited_people">
+            </div>
+            <div class="flex-1">
+                <label class="block font-medium text-sm text-black" for="total_votes">
+                    Celkový počet hlasov
+                </label>
+                <input disabled=""
+                    class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm mt-1 block w-full cursor-not-allowed"
+                    id="total_votes" name="total_votes" type="text" value="{{ $votes }}"
+                    required="required" autocomplete="total_votes">
+            </div>
+
+            <div class="flex-1">
+                <label class="block font-medium text-sm text-black" for="max_votes_per_day">
+                    Maximálny počet hlasov za deň
+                </label>
+                <input disabled=""
+                    class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm mt-1 block w-full cursor-not-allowed"
+                    id="max_votes_per_day" name="max_votes_per_day" type="text" value="{{ $max_votes_per_day }}"
+                    required="required" autocomplete="max_votes_per_day">
+            </div>
+
+            <div class="flex-1">
+                <label class="block font-medium text-sm text-black" for="weight_per_vote">
+                    Vaha jednotlivého hlasu
+                </label>
+                <input disabled=""
+                    class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm mt-1 block w-full cursor-not-allowed"
+                    id="weight_per_vote" name="weight_per_vote" type="text" value="{{ $weight_per_vote }}"
+                    required="required" autocomplete="weight_per_vote">
+            </div>
         </div>
-    </div>
 </section>
