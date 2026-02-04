60a8a382942b500df8b9acc3073cf47a967f1bf0
Peter Kvokačka <peter.kvokacka11@gmail.com>
2026-02-04 15:42:51 +0100

    Refactor invite link component to enhance button feedback and improve layout

--- resources/views/profile/partials/invite-link.blade.php
+++ resources/views/profile/partials/invite-link.blade.php
@@ -6,10 +6,24 @@
             zaregistruje pomocou tohto odkazu, vám poskytne extra výhody.
         </p>
     </div>
-    <div class="relative w-[60%]">
+    <div class="relative w-full lg:max-w-[60%]">
         <input disabled class="mt-1 block w-full rounded-md border-gray-300 bg-gray-100 cursor-not-allowed shadow-sm"
             value="{{ url('/r/' . $invite_code) }}">
-        <button class="copy-btn absolute right-2 top-2 rounded bg-gray-200 px-2 py-1 text-sm font-semibold text-gray-700 hover:bg-gray-300" data-copy="{{ url('/r/' . $invite_code) }}">Kopírovať odkaz</button>
+        <button
+            class="copy-btn absolute right-2 top-1/2 -translate-y-1/2 rounded bg-gray-200 px-2 py-1 text-sm font-semibold text-gray-700 hover:bg-gray-300"
+            data-copy="{{ url('/r/' . $invite_code) }}">
+            <p class="hidden md:block">Kopírovať odkaz</p>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="size-6 block md:hidden">
+                <path id="svgCopy" stroke-linecap="round" stroke-linejoin="round"
+                    d="M15.666 3.888A2.25 2.25 0 0 0 13.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 0 1-.75.75H9a.75.75 0 0 1-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 0 1-2.25 2.25H6.75A2.25 2.25 0 0 1 4.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 0 1 1.927-.184" />
+                <path id="svgCopied" class="hidden" stroke-linecap="round" stroke-linejoin="round"
+                    d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0 1 18 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3 1.5 1.5 3-3.75" />
+
+
+            </svg>
+
+        </button>
     </div>
 
 </div>
@@ -18,9 +32,15 @@
     document.querySelectorAll('.copy-btn').forEach(btn => {
         btn.addEventListener('click', () => {
             navigator.clipboard.writeText(btn.dataset.copy);
-            btn.textContent = 'Odkaz skopírovaný!';
+            btn.children[0].textContent = 'Odkaz skopírovaný!';
             setTimeout(() => {
-                btn.textContent = 'Kopírovať odkaz';
+                btn.children[0].textContent = 'Kopírovať odkaz';
+            }, 2000);
+            document.getElementById('svgCopy').classList.toggle('hidden');
+            document.getElementById('svgCopied').classList.toggle('hidden');
+            setTimeout(() => {
+                document.getElementById('svgCopy').classList.toggle('hidden');
+                document.getElementById('svgCopied').classList.toggle('hidden');
             }, 2000);
         });
     });
