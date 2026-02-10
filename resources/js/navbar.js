8641922b78943ea0427e4dd0722e3d4ff2c7a5aa
Peter Kvokačka <peter.kvokacka11@gmail.com>
2026-02-10 15:59:05 +0100

    Add input handling and password visibility toggle functionality
    - Implemented input handling for text, email, and password fields to add/remove classes based on input value.
    - Added password visibility toggle feature for password and password confirmation fields with SVG icon updates.
    - Created hamburger menu functionality to toggle visibility of navigation elements.
    - Added dropdown handling for language selection in both desktop and mobile views, including show/hide functionality.

---
+++ resources/js/navbar.js
@@ -0,0 +1,43 @@
+document.addEventListener('click', function (e) {
+    const dropdown = document.getElementById('lang-dropdown');
+    const dropdownMobile = document.getElementById('lang-dropdown-mobile');
+
+    if (dropdown) {
+        const wrapper = dropdown.parentElement;
+
+        if (!wrapper.contains(e.target)) {
+            dropdown.classList.add('hidden');
+            dropdown.classList.remove('flex');
+        }
+    }
+
+    if (dropdownMobile) {
+        const wrapperMobile = dropdownMobile.parentElement;
+
+        if (!wrapperMobile.contains(e.target)) {
+            dropdownMobile.classList.add('hidden');
+            dropdownMobile.classList.remove('flex');
+        }
+    }
+});
+
+window.showDropdown = function (id) {
+    var dropdown = document.getElementById(id);
+    dropdown.classList.remove("hidden");
+}
+
+window.hideDropdown = function (id) {
+    var dropdown = document.getElementById(id);
+    dropdown.classList.add("hidden");
+}
+
+window.toggleDropdown = function (id) {
+    var dropdown = document.getElementById(id);
+    if (dropdown.classList.contains("hidden")) {
+        dropdown.classList.remove("hidden");
+        dropdown.classList.add("flex");
+    } else {
+        dropdown.classList.add("hidden");
+        dropdown.classList.remove("flex");
+    }
+}
