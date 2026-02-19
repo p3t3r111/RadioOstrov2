1e3d4bdbb64bdb6fba7d5d2496288f726f82dec4
Peter Kvokačka <peter.kvokacka11@gmail.com>
2026-02-19 17:21:24 +0100

    fix: remove console logs for cleaner code in navbar.js

--- resources/js/navbar.js
+++ resources/js/navbar.js
@@ -45,9 +45,7 @@ window.toggleDropdown = function (id) {
 const navbar = document.getElementById("mainNav");
 const dropdown = document.getElementById("dropdown").parentElement;
 
-console.log(navbar, dropdown);
 
 if (navbar && dropdown) {
-    console.log(navbar.offsetHeight);
     dropdown.style.top = `${navbar.offsetHeight}px`;
 }
