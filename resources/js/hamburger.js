b2de2bed56d759c62f32fc2c6d1078feccb1837b
Peter Kvokačka <peter.kvokacka11@gmail.com>
2026-02-19 13:00:41 +0100

    fix: wrap hamburger click event listener in a null check for improved stability

--- resources/js/hamburger.js
+++ resources/js/hamburger.js
@@ -4,19 +4,21 @@ const hamburgerOpen = document.getElementById("hamburger-open");
 const hamburgerClose = document.getElementById("hamburger-close");
 const mainNavBar = document.getElementById("mainNavBar2");
 
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("is-active");
-    if (hamburger.classList.contains("is-active")) {
-        hamburgerOpen.classList.add("hidden");
-        hamburgerClose.classList.add("block");
-        hamburgerClose.classList.remove("hidden");
-        hamburgerOpen.classList.remove("block");
-    } else {
-        hamburgerOpen.classList.add("block");
-        hamburgerClose.classList.add("hidden");
-        hamburgerClose.classList.remove("block");
-        hamburgerOpen.classList.remove("hidden");
-    }
-    mainNavBar.classList.toggle("hidden");
-    mainNavBar.classList.toggle("colnav");
-});
+if (hamburger) {
+    hamburger.addEventListener("click", () => {
+        hamburger.classList.toggle("is-active");
+        if (hamburger.classList.contains("is-active")) {
+            hamburgerOpen.classList.add("hidden");
+            hamburgerClose.classList.add("block");
+            hamburgerClose.classList.remove("hidden");
+            hamburgerOpen.classList.remove("block");
+        } else {
+            hamburgerOpen.classList.add("block");
+            hamburgerClose.classList.add("hidden");
+            hamburgerClose.classList.remove("block");
+            hamburgerOpen.classList.remove("hidden");
+        }
+        mainNavBar.classList.toggle("hidden");
+        mainNavBar.classList.toggle("colnav");
+    });
+}
