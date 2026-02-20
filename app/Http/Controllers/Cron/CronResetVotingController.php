2865f846ecc537bcabdc27e1c82b5a14fa2e5030
Peter Kvokačka <peter.kvokacka11@gmail.com>
2026-02-20 11:07:34 +0100

    fix: update all_time_points calculation for voted users in CronResetVotingController

--- app/Http/Controllers/Cron/CronResetVotingController.php
+++ app/Http/Controllers/Cron/CronResetVotingController.php
@@ -6,13 +6,17 @@ use App\Actions\cron\AddNewSongsToPlaylist;
 use App\Actions\cron\GenerateNewVotingSongs;
 use App\Actions\cron\StoreVotingSongs;
 use App\Http\Controllers\Controller;
+use App\Models\User;
 
 class CronResetVotingController extends Controller
 {
     public function index()
     {
         sleep(50);
-
+        $votedUsers = User::where('voted', '>', 0)->each(function ($user) {
+            $user->all_time_points += $user->voted * config('app.vote_point_value');
+            $user->save();
+        });
         StoreVotingSongs::execute();
         GenerateNewVotingSongs::execute();
         AddNewSongsToPlaylist::execute();
