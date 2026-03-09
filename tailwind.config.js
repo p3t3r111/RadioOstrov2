e7f703de81e42e0682ffeca6f5549f9c191e92d5
Peter Kvokačka <peter.kvokacka11@gmail.com>
2026-03-09 08:25:40 +0100

    fix: correct customPulse animation keyframes for consistent scaling

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,7 @@ export default {
                     "100%": { opacity: "1" },
                 },
                 customPulse: {
-                    "1%, 99%": { color: "#000000", scale: "1" },
+                    "0%, 100%": { scale: "1" },
                     "50%": { color: "#49FF00", scale: "1.1" },
                 },
             },
