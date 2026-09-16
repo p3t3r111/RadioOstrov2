9651e8849f35b05e7de5c57de22cc61d2f715ef5
Peter Kvokačka <peter.kvokacka11@gmail.com>
2026-09-16 22:14:04 +0200

    posible fix holiday systemu

--- app/Actions/isHolidays.php
+++ app/Actions/isHolidays.php
@@ -16,12 +16,13 @@ class isHolidays
 
         if ($holiday->isNotEmpty()) {
             $voting = Voting_dates::activeVoting();
-
             $startDate = Carbon::parse($voting->from)->setTime(14, 0);
             $endDate = Carbon::parse($voting->to)->setTime(14, 0);
+            $holidayStartDate = Carbon::parse($holiday->first()->start_date)->subDay()->setTime(14, 0);
+            $holidayEndDate = Carbon::parse($holiday->first()->end_date)->setTime(14, 0);
             $now = now();
 
-            if ($now->between($startDate, $endDate)) {
+            if ($now->between($startDate, $endDate) && $now->between($holidayStartDate, $holidayEndDate)) {
                 return true;
             }
         }
